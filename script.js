55bd3e27bfe4160806d545851e4746e32845e542
Narenprakash25 <73632920+Narenprakash25@users.noreply.github.com>
2023-08-15 00:12:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -56,7 +56,7 @@ ScrollReveal().reveal('.home-content p, .about-content', { origin:'right'});
 
 
 const typed=new Typed('.multiple-text',{
-    strings:['Business Analyst','Data Analyst','Data Scientist','Frontend Developer'],
+    strings:['Business Analyst','Data Analyst','Data Scientist','Web Developer'],
     typeSpeed:100,
     backSpeed:100,
     backDelay:1000,
